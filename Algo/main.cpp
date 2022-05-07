d5256bc43ec5e872209d73d4981249583a2ac656
Aman <aman.arabzadeh98@gmail.com>
2022-05-07 12:32:25 +0200

    Initial Commit

---
+++ Algo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algo
+//
+//  Created by Aman arabzadeh on 2022-05-07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
